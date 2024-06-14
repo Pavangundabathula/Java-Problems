9bdf6d9693b24fa7d6d3cbfcc89a036ed46f173b
Gundabathula Rama Yesu Pavan <119726124+Pavangundabathula@users.noreply.github.com>
2024-06-14 21:29:03 +0530

    Add files via upload

---
+++ Array_without_loops.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Array_without_loops {
+    public static void main(String[] args){
+        int[] arr={1,2,3,5,64,23,32};
+        System.out.println(Arrays.toString(arr));
+    }   
+}
