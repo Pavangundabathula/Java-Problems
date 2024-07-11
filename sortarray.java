b0887704a63d9c24e0a6afbfc3c6cea6f1009ee2
Gundabathula Rama Yesu Pavan <pavangundabathula143@gmail.com>
2024-07-11 21:44:35 +0530

    Add files via upload

---
+++ sortarray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class sortarray {
+    public static void main(String[] args){
+        int[] arr={1,3,4,5,2,1,9,0,-1};
+        Arrays.sort(arr);
+        String[] arr1={"Pavan","Sciro","pojskj","hdgtufwb","dsugh"};
+        Arrays.sort(arr);
+        for(String str:arr1){
+            System.out.println(str);
+        }
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+    
+}
